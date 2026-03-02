d79ed8dc65bda93f465fcb937714f08fe5d4a628
ttenchurin <t.tenchurin@list.ru>
2026-03-02 20:03:25 +0000

    third commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,28 @@
-{
-  "env": {
-    "node": true,
-    "es2021": true
-  },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  },
-  "rules": {
-    "no-unused-vars": ["error", {
-      "argsIgnorePattern": "^_",
-      "varsIgnorePattern": "^_"
-    }]
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        // Глобальные переменные для Node.js
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        module: "readonly",
+        require: "readonly",
+        Buffer: "readonly",
+        console: "readonly",
+        exports: "writable",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }]
+    }
   }
-}
+];
